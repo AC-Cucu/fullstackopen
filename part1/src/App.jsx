09c54bb1468e2b1c0075787004cced30ebb624f4
AC-Cucu <alexandrucristian.cucuruzeanu@gmail.com>
2025-07-10 15:35:24 +0200

    Part1 - React Introduction - Using fragment anotaion instead of a div one;

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,8 +1,9 @@
 const Hello = (props) => {
+  console.log(props)
   return (
-    <div>
+    <>
       <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
+    </>
   )
 }
 
@@ -12,12 +13,12 @@ const App = () => {
   const age = 10
 
   return (
-    <div>      
+    <>      
       <h1>Greetings</h1>      
       
       <Hello name="Maya" age={26+10}/>
       <Hello name={name} age={age}/>
-    </div>
+    </>
   )
 }
 
