45e2830861802f95b1f9c9fc173f44de6f85e418
AC-Cucu <alexandrucristian.cucuruzeanu@gmail.com>
2025-07-10 15:37:21 +0200

    Part1 - React Introduction - Objects are not valid as a React child example;

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -9,15 +9,16 @@ const Hello = (props) => {
 
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
 
   return (
     <>      
       <h1>Greetings</h1>      
-      
-      <Hello name="Maya" age={26+10}/>
-      <Hello name={name} age={age}/>
+      <p>{friends[0]}</p>
+      <p>{friends[1]}</p>
     </>
   )
 }
