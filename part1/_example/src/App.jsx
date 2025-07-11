4e1edc6a6c20c0e7a6b77a876dcfc365f555f8e8
Alexandru Cristian Cucuruzeanu <alexandrucristian.cucuruzeanu@gmail.com>
2025-07-11 12:29:10 +0200

    Part1 - React Introduction - Course Info - separating event handlers into their own functions example;

--- part1/_example/src/App.jsx
+++ part1/_example/src/App.jsx
@@ -4,19 +4,17 @@ const App = () => {
 
   const [ counter, setCounter ] = useState(0)
 
-  // const handleClick = () => {
-  //   console.log('clicked')
-  // }
-
+  const increaseByOne = () => setCounter(counter + 1)  
+  const setToZero = () => setCounter(0)
   console.log('rendering...', counter)
 
   return (
     <>
       <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
+      <button onClick={increaseByOne}>
         plus
       </button>    
-      <button onClick={() => setCounter(0)}>
+      <button onClick={setToZero}>
         zero
       </button>      
     </>
