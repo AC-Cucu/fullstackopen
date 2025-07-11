256dafe352995304c5ad59fc0b007560863d3279
Alexandru Cristian Cucuruzeanu <alexandrucristian.cucuruzeanu@gmail.com>
2025-07-11 12:13:58 +0200

    Part1 - React Introduction - Course Info - Page re-rendering example;

--- part1/_example/src/main.jsx
+++ part1/_example/src/main.jsx
@@ -1,5 +1,19 @@
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 
-import App from './App.jsx'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(<App/>)
+let counter = 1
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const refresh = () => {
+  root.render(
+    <App counter={counter} />
+  )
+}
+
+refresh()
+counter += 1
+refresh()
+counter += 1
+refresh()
