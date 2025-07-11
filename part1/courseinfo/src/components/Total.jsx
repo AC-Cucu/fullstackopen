4cf444979a1390db9dfc52366c0b6ed00464f3fc
Alexandru Cristian Cucuruzeanu <alexandrucristian.cucuruzeanu@gmail.com>
2025-07-11 11:05:08 +0200

    Part1 - React Introduction - Course Info - Adding an array and adjust components affected;

--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,9 +1,9 @@
-const Total = ({props}) => {
+const Total = (props) => {
     console.log('Total component rendered')
     console.log(props)
     return (
         <>
-            <p>Number of exercises {props.total}</p>
+            <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
         </>
     )
 }
