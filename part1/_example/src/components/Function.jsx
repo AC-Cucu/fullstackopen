ad44e27409affe3d62fe23ddbb3660db9a52a5da
alexandru.cucuruzeanu <alexandrucristian.cucuruzeanu@gmail.com>
2025-07-21 13:32:34 +0200

    Part1 - React Introduction - A more complex State - Adding example of a function that returns another function working with a state variable;

--- part1/_example/src/components/Function.jsx
+++ part1/_example/src/components/Function.jsx
@@ -4,15 +4,31 @@ const Function = () => {
     console.log('Function component rendered')
     const [value, setValue] = useState(10)
 
-    const hello = () => {
-        const handler = () => console.log('hello world')
-        return handler
+    // This is an example of a function that returns another function
+    // For example, the function `hello('react')` will return () => {  console.log('hello', 'react')}
+    // const hello = (who) => () => {
+    //     console.log('hello', who)
+    // }
+
+    const setToValue = (newValue) => () => {
+        console.log('setting value to', newValue)
+        setValue(newValue)
     }
+    // We could also declare the function like this:
+    // const setToValue = (newValue) => {
+    //     console.log('value now', newValue)
+    //     setValue(newValue)
+    // }
+    //And in our JSX we would use it like this:
+    // <button onClick={() => setToValue(1000)}>thousand</button>
+
 
     return (
             <div>
                 {value}
-                <button onClick={hello()}> button </button>
+                <button onClick={setToValue(1000)}>thousand</button>
+                <button onClick={setToValue(0)}>reset</button>
+                <button onClick={setToValue(value + 1)}>increment</button>
             </div>
 
     )
