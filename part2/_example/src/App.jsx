8b46d9a2f788264c5890be1f8473c16d35421ac6
alexandru.cucuruzeanu <alexandrucristian.cucuruzeanu@gmail.com>
2025-07-22 11:18:28 +0200

    Part2 - React Introduction - Adding Note component to the example Notes App;

--- part2/_example/src/App.jsx
+++ part2/_example/src/App.jsx
@@ -1,15 +1,14 @@
-const App = (props) => {
-  const { notes } = props
+import Note from "./components/Note"
 
+const App = ({notes}) => {
+  console.log('Notes:', notes)
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {
           notes.map(note => 
-            <li key={note.id}>
-              {note.content}
-            </li>
+            <Note key={note.id} note={note} />
           )
         }
       </ul>
