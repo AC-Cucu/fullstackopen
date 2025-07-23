14f24515c3fef2e5cd06476f12053c24f5823a0d
alexandru.cucuruzeanu <alexandrucristian.cucuruzeanu@gmail.com>
2025-07-23 12:48:44 +0200

    Part2 - React Introduction - Example - Adding style and new components, including an Notification component;

--- part2/_example/src/App.jsx
+++ part2/_example/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react"
+
 import Note from "./components/Note"
+import Notification from "./components/Notification"
+import Footer from "./components/Footer"
+
 import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')  
 
   const hook = () => {
     console.log('effect')
@@ -77,9 +82,12 @@ const App = () => {
       })
       .catch(error => {
         console.error("Updating notes error:", error)
-        alert(
+        setErrorMessage(
           `the note '${note.content}' was already deleted from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -87,6 +95,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <button onClick={handleClickShowAll}>
         show {showAll ? 'important' : 'all'}
       </button>
@@ -105,6 +114,7 @@ const App = () => {
         <input name="note" value={newNote} onChange={handleNoteChange} />
         <button type="submit">Add Note</button>
       </form>
+      <Footer />
     </div>
   )
 }
